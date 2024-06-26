48816cb85635c91c06d9d976a11175efe87c6cf8
rinterron <144239782+ssmitee@users.noreply.github.com>
2024-06-26 13:01:36 +0200

    Add files via upload

---
+++ WpfApp/WpfApp/Fraction.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class Fraction : Pair
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero");
+            }
+
+            Numerator = numerator;
+            Denominator = denominator;
+            Normalize();
+        }
+
+        private void Normalize()
+        {
+            if (Denominator < 0)
+            {
+                Numerator = -Numerator;
+                Denominator = -Denominator;
+            }
+        }
+
+        public override Pair Add(Pair other)
+        {
+            if (other is Fraction otherFraction)
+            {
+                int newNumerator = Numerator * otherFraction.Denominator + otherFraction.Numerator * Denominator;
+                int newDenominator = Denominator * otherFraction.Denominator;
+                return new Fraction(newNumerator, newDenominator);
+            }
+            throw new ArgumentException("Invalid type of argument for Add operation");
+        }
+
+        public override Pair Subtract(Pair other)
+        {
+            if (other is Fraction otherFraction)
+            {
+                int newNumerator = Numerator * otherFraction.Denominator - otherFraction.Numerator * Denominator;
+                int newDenominator = Denominator * otherFraction.Denominator;
+                return new Fraction(newNumerator, newDenominator);
+            }
+            throw new ArgumentException("Invalid type of argument for Subtract operation");
+        }
+
+        public override Pair Multiply(Pair other)
+        {
+            if (other is Fraction otherFraction)
+            {
+                int newNumerator = Numerator * otherFraction.Numerator;
+                int newDenominator = Denominator * otherFraction.Denominator;
+                return new Fraction(newNumerator, newDenominator);
+            }
+            throw new ArgumentException("Invalid type of argument for Multiply operation");
+        }
+
+        public override Pair Divide(Pair other)
+        {
+            if (other is Fraction otherFraction)
+            {
+                if (otherFraction.Numerator == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                int newNumerator = Numerator * otherFraction.Denominator;
+                int newDenominator = Denominator * otherFraction.Numerator;
+                return new Fraction(newNumerator, newDenominator);
+            }
+            throw new ArgumentException("Invalid type of argument for Divide operation");
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+    }
+
+}
---
+++ WpfApp/WpfApp/Money.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class Money : Pair
+    {
+        public int Rubles { get; set; }
+        public int Kopecks { get; set; }
+
+        public Money(int rubles, int kopecks)
+        {
+            Rubles = rubles;
+            Kopecks = kopecks;
+            Normalize();
+        }
+
+        private void Normalize()
+        {
+            Rubles += Kopecks / 100;
+            Kopecks %= 100;
+            if (Kopecks < 0)
+            {
+                Kopecks += 100;
+                Rubles -= 1;
+            }
+        }
+
+        public override Pair Add(Pair other)
+        {
+            if (other is Money otherMoney)
+            {
+                return new Money(Rubles + otherMoney.Rubles, Kopecks + otherMoney.Kopecks);
+            }
+            throw new ArgumentException("Invalid type of argument for Add operation");
+        }
+
+        public override Pair Subtract(Pair other)
+        {
+            if (other is Money otherMoney)
+            {
+                return new Money(Rubles - otherMoney.Rubles, Kopecks - otherMoney.Kopecks);
+            }
+            throw new ArgumentException("Invalid type of argument for Subtract operation");
+        }
+
+        public override Pair Multiply(Pair other)
+        {
+            if (other is Money otherMoney)
+            {
+                int totalKopecks1 = Rubles * 100 + Kopecks;
+                int totalKopecks2 = otherMoney.Rubles * 100 + otherMoney.Kopecks;
+                int resultKopecks = totalKopecks1 * totalKopecks2 / 100;
+                return new Money(resultKopecks / 100, resultKopecks % 100);
+            }
+            throw new ArgumentException("Invalid type of argument for Multiply operation");
+        }
+
+        public override Pair Divide(Pair other)
+        {
+            if (other is Money otherMoney)
+            {
+                int totalKopecks1 = Rubles * 100 + Kopecks;
+                int totalKopecks2 = otherMoney.Rubles * 100 + otherMoney.Kopecks;
+                if (totalKopecks2 == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                int resultKopecks = (totalKopecks1 * 100) / totalKopecks2;
+                return new Money(resultKopecks / 100, resultKopecks % 100);
+            }
+            throw new ArgumentException("Invalid type of argument for Divide operation");
+        }
+
+        public override string ToString()
+        {
+            return $"{Rubles} руб. {Kopecks} коп.";
+        }
+    }
+}
